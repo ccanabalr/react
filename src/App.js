83f85882a6739e3a0cc4b6c231c89df2ac009b20
ccanabalr <ccanabalr@gmail.com>
2020-02-28 17:16:49 -0500

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import TarjetaFruta from './componentes/TarjetaFruta'
+const App = () => (
+    <div>
+        <TarjetaFruta name='Sandia' price={5.00} />
+        <TarjetaFruta name='Naranja' price={2.00} />
+        <TarjetaFruta name='Kiwi' price={9.00} />
     </div>
-  );
-}
+)
 
-export default App;
+export default App
